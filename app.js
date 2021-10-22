3b28cda88e4a919738ef6048dca6cc0ee7e72666
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-22 16:28:30 +0530

    space station 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
     //!setView is,3
     //! latitude , longitude and initial zoom level
-    var map = L.map('map').setView([0, 0], 0);
+    var map = L.map('map').setView([0, 0], 1);
     //!Add the layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
         .addTo(map);
@@ -8,12 +8,12 @@
     //!Change the icon
     var myIcon = L.icon({
         iconUrl: './assets/iss.png',
-        iconSize: [38, 32],
+        iconSize: [50, 50],
 
     });
     var pathIcon = L.icon({
         iconUrl: 'https://www.svgrepo.com/show/344736/dot.svg',
-        iconSize: [20, 20],
+        iconSize: [45, 45],
 
     });
     //!latitude , longitude 
@@ -24,12 +24,22 @@
     const fetchSpaceStationDetails = async() => {
         const res = await fetch('https://api.wheretheiss.at/v1/satellites/25544')
         const data = await res.json()
+        console.log(data)
+        document.createElement('object').innerHTML = data
         const { latitude, longitude } = data
+        const long = data.longitude
+        const lati = data.latitude
+        const velo = data.velocity
+        const altiu = data.altitude
+
         // console.log(latitude, longitude)
         marker.setLatLng([latitude, longitude])
             //!Add the path icon
         L.marker([latitude, longitude], { icon: pathIcon }).addTo(map)
-
+        document.getElementById('long').innerHTML = long.toFixed(4)
+        document.getElementById('lat').innerHTML = lati.toFixed(4)
+        document.getElementById('vel').innerHTML = velo.toFixed(0) + "km/Hr"
+        document.getElementById('alt').innerHTML = altiu.toFixed(4)
     }
     fetchSpaceStationDetails()
         // console.log(L)
